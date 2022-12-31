99bb004b19ccff469b8272dff1891aebc9b6640b
Kelamir <kelamir@protonmail.com>
2022-12-31 03:35:23 +0200

    Fetch popular movies from api

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script context="module">
+    // ^ for server side loading of api requests
+
+    let api_key = ""
+
+    // async means waits for data to be available
+    export async function load({fetch }) {
+        const res = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${api_key}`);
+        const data = await res.json(); // formats it to json
+        console.log(data);
+        if(res.ok) {
+            return {
+                props: { popular: data.results }
+            }
+        }
+    }
+
+</script>
