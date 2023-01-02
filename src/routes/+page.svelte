9b4a275c643ac20bdb34b181ecb56ee9b28967c1
Kelamir <kelamir@protonmail.com>
2023-01-02 10:44:02 +0200

    Remove vulnerability that returns the API key to the console.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,7 @@
 <script context="module">
     // ^ for server side loading of api requests
 
-
     // async means waits for data to be available
-    console.log(`${import.meta.env.VITE_API}`)
     export async function load({fetch }) {
         const res = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${import.meta.env.VITE_API} `);
         const data = await res.json(); // formats it to json
